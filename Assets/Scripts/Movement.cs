44ceaec6b95290746914cd76f437909447ed9360
Jáchym Tomášek <westerofwest@gmail.com>
2016-11-27 12:21:17 +0100

    Add movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    public float acceleration;
+    public float turning;
+    public float breaks;
+    private Vector2 moveDir = Vector2.zero;
+    private float actualSpeed = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+
+        }
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
