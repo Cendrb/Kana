2427d8e4a1cb09980d20f309e87277dd3f257429
Cendrb <cendrb@gmail.com>
2016-11-27 13:12:18 +0100

    Add scene launcher script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenuScript : MonoBehaviour {
+public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+    private void Start () {
+	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+    private void Update () {
+	    
 	}
 }
---
+++ Assets/Scripts/SceneLauncher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLauncher : MonoBehaviour
+{
+    public Texture loadingOverlayTexture;
+
+    private AsyncOperation currentOperation;
+    private GUIStyle guiStyle;
+
+    public void Launch(string sceneName)
+    {
+        if (currentOperation == null || currentOperation.isDone)
+        {
+            currentOperation = SceneManager.LoadSceneAsync(sceneName);
+            currentOperation.allowSceneActivation = true;
+        }
+        else
+        {
+            Debug.Log("Already opening a scene");
+        }
+    }
+
+    private void Start()
+    {
+        if (loadingOverlayTexture == null)
+            throw new ArgumentException("loadingOverlayTexture cannot be null");
+
+        guiStyle = new GUIStyle {fontSize = 69, alignment = TextAnchor.LowerRight};
+    }
+
+    private void OnGUI()
+    {
+        if (currentOperation != null && !currentOperation.isDone)
+        {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), loadingOverlayTexture);
+            GUI.Label(new Rect(Screen.width - 400, Screen.height - 100, 400, 100), "Loading...", guiStyle);
+        }
+    }
+}
