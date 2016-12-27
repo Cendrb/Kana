d2e06b5eaca9740aa3dfd0f0c5fbca43dbc0136f
Cendrb <cendrb@gmail.com>
2016-12-27 16:34:04 +0100

    Move Model to an appropriate namespace

--- Assets/Scripts/PartLoading/Objects/Model.cs
+++ Assets/Scripts/PartLoading/Objects/Model.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Assets.Scripts.PartLoading.Objects;
+﻿using System.Collections.Generic;
 
-namespace Assets.Scripts.PartLoading
+namespace Assets.Scripts.PartLoading.Objects
 {
     public class Model
     {
