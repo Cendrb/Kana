16549d494d5d2f11de3054394fe07084b0878a7a
Čeněk Drbal <cendrb@gmail.com>
2017-04-16 11:10:09 +0200

    Add broken vehicle to the main scene

---
+++ Assets/Scripts/SinglePlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.GameResources;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class SinglePlayerScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameObject vehicleGO = new GameObject("Vehicle");
+            Vehicle vehicle = new Vehicle();
+            vehicle.Instantiate(vehicleGO, 1f);
+            vehicle.Deserialize(JObject.Parse(File.ReadAllText(@"c:\temp\kana.json")));
+        }
+    }
+}
